c1c7b293aa38a7e352aba0c3aa02e534dbd3a1ba
David Gjorgoski <david_gjorgoski@hotmail.com>
2022-01-04 15:40:06 +0100

    - Add React Router library - Move login form to /login route - Make /login the default route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import LoginForm from './LoginForm';
 import './App.css';
+import {Route, Routes, Navigate} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <LoginForm />
+        <Routes>
+            <Route path="/" element={<Navigate to="/login"/>}/>
+            <Route path="/login" element={<LoginForm/>}/>
+        </Routes>
     </div>
   );
 }
