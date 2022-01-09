843bf9ba0a0724c7930d0460fe7527b52fccee3f
David Gjorgoski <david_gjorgoski@hotmail.com>
2022-01-09 15:58:16 +0100

    - Added logout functionality

--- src/DashboardVisual.tsx
+++ src/DashboardVisual.tsx
@@ -13,7 +13,7 @@ const DashboardVisual = () => {
                 {user?.firstName} {user?.lastName}
             </NavbarText>
             <NavItem>
-                <NavLink>
+                <NavLink href="/" onClick={() => {document.cookie="token=; expires=Thu, 01 Jan 1970 00:00:00 UTC;"}}>
                     Log Out
                 </NavLink>
             </NavItem>
